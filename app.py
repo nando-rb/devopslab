258986d9c484ef0e39ad8bc8716021083626cb77
nando-rb <nandorb.2021@gmail.com>
2021-04-09 01:09:04 +0000

    Bug Removido

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 
 csrf = CSRFProtect(app)
 
+@app.route("/")
+def pagina_inicial():
+    return "Laboratório Pipeline DevOps"
+
 if __name__ == '__main__':
     app.run(debug=True)
