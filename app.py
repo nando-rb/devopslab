b7e2a0002fae15479105fb90c9a9a3edda466c39
nando-rb <nandorb.2021@gmail.com>
2021-04-09 01:06:37 +0000

    Bug Removido

--- app.py
+++ app.py
@@ -5,19 +5,5 @@ app = Flask(__name__)
 
 csrf = CSRFProtect(app)
 
-@app.route("/")
-def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
-
 if __name__ == '__main__':
     app.run(debug=True)
